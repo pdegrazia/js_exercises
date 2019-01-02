0d38eeb76d74a1373f7cf9885586c9b364eff8e9
Paolo <p.degrazia@gmail.com>
2019-01-02 00:14:41 +0000

    break continue

---
+++ continue_break.js
@@ -0,0 +1,14 @@
+for (var i=0; i<=10; i++){
+    if (i == 7){
+        break;
+    }
+    console.log(i);
+}
+
+for (var i=0; i<=10; i++){
+    if (i == 7){
+        continue;
+    }
+    console.log(i);
+}
+
